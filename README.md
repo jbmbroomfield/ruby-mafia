75737b99f5bfe0a79e37141ac3601e62cb86e401
jim <jbmbroomfield@gmail.com>
2021-08-19 14:37:49 +1000

    Added setup folder in models

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mafia-actions
+# mafia
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/DataClass.rb'
 
-['admin', 'games', 'game'].each do |directory|
+['admin', 'games', 'setup', 'game'].each do |directory|
     Dir[File.join(__dir__, '..', 'lib', 'models', directory, '*.rb')].each { |file| require_relative file }
 end
 
